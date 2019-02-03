08921bf9add56d8ac26ca822d08505bf70ac85da
utkuumur <uacikalin@etu.edu.tr>
2019-02-03 17:29:47 +0300

    skip auth

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -1,4 +1,5 @@
 class RoutesController < ApplicationController
+  skip_before_action :verify_authenticity_token
   before_action :set_route, only: [:show, :edit, :update, :destroy]
 
   # GET /routes
