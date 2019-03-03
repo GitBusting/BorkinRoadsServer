cde168f3aa419b53644bb9e91e411f395a838ae0
utkuumur <uacikalin@etu.edu.tr>
2019-03-03 18:55:15 +0300

    created user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_15_192057) do
+ActiveRecord::Schema.define(version: 2019_03_03_155404) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,8 @@ ActiveRecord::Schema.define(version: 2019_02_15_192057) do
     t.datetime "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "route_id"
+    t.index ["route_id"], name: "index_route_details_on_route_id"
   end
 
   create_table "routes", force: :cascade do |t|
@@ -54,6 +56,14 @@ ActiveRecord::Schema.define(version: 2019_02_15_192057) do
     t.boolean "nearPark"
     t.boolean "favourite"
     t.integer "numberOfTimesUsed"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
