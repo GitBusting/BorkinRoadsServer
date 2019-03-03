213651b6c1c9360a8f46f3e728f256db77339aa6
utkuumur <uacikalin@etu.edu.tr>
2019-03-03 23:06:23 +0300

    block access untill auth

--- app/controllers/route_details_controller.rb
+++ app/controllers/route_details_controller.rb
@@ -1,5 +1,5 @@
 class RouteDetailsController < ApplicationController
-  skip_before_action :verify_authenticity_token
+  #skip_before_action :verify_authenticity_token
   before_action :set_route_detail, only: [:show, :edit, :update, :destroy]
 
   # GET /route_details
--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -1,5 +1,5 @@
 class RoutesController < ApplicationController
-  skip_before_action :verify_authenticity_token
+  #skip_before_action :verify_authenticity_token
   before_action :set_route, only: [:show, :edit, :update, :destroy]
 
   # GET /routes
